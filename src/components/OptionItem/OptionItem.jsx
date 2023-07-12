033559e31f10f8fbd7112628e45fd514f8c6030c
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-12 14:39:45 +0200

    Refactors OptionItem from class to hook

--- src/components/OptionItem/OptionItem.jsx
+++ src/components/OptionItem/OptionItem.jsx
@@ -1,19 +1,15 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { OptionBtn } from '../FeedbackOptions/FeedbackOptions.styled';
 
-export class OptionItem extends Component {
-  render() {
-    const { option, onLeaveFeedback } = this.props;
-    return (
-      <li>
-        <OptionBtn type="button" onClick={onLeaveFeedback} name={option}>
-          {option}
-        </OptionBtn>
-      </li>
-    );
-  }
-}
+export const OptionItem = ({ option, onLeaveFeedback }) => {
+  return (
+    <li>
+      <OptionBtn type="button" onClick={onLeaveFeedback} name={option}>
+        {option}
+      </OptionBtn>
+    </li>
+  );
+};
 
 OptionItem.propTypes = {
   option: PropTypes.string.isRequired,
