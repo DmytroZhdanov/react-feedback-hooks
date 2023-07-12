f09767f4791bca784efb25fb71a93895b0356b71
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-12 14:41:29 +0200

    Refactors Notification from class to hook

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,14 +1,9 @@
-import { Component } from 'react';
 import { Message } from './Notification.styled';
 import PropTypes from 'prop-types';
 
-
-export class Notification extends Component {
-  render() {
-    const { message } = this.props;
-    return <Message>{message}</Message>;
-  }
-}
+export const Notification = ({ message }) => {
+  return <Message>{message}</Message>;
+};
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
